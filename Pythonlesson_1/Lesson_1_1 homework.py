324ff623f6801529fbd8ae98446156f31122f4de
prakh <solomonizrailevic@gmail.com>
2026-01-30 15:08:14 +0300

    homework 1

---
+++ Pythonlesson_1/Lesson_1_1 homework.py
@@ -0,0 +1,65 @@
+# str_1 = "Iaroslav"
+# name = str_1
+# age = 24
+# height = 1.89
+# print (name)
+# print (age)
+# print (height)
+#
+#
+# x = 10
+# print(type(x))
+# x = 25.5
+# print(type(x))
+# str_2 = "python"
+# x = str_2
+# print(x)
+# print(type(x))
+
+# a = 7
+# b = a
+# print(a, b)
+# a = 10
+# print(b)
+
+# x = y = z = 100
+# print(x, y, z)
+# print(id(x))
+# print(id(y))
+# print(id(z))
+# x, y, z = 101, 102, 103
+# print(x, y, z)
+# print(id(x))
+# print(id(y))
+# print(id(z))
+#
+# a = 5
+# b = 10
+# a, b = b, a
+# print(a, b)
+
+# true = 2
+# if = 3
+# print = 4
+# print(true, if, print)
+# import keyword
+# print(keyword.kwlist)
+
+# var1 = 42
+# var2 = 3.14
+# var3 = "Hello"
+# print(type(var1))
+# print(type(var2))
+# print(type(var3))
+# var1 = var3
+# print(type(var1))
+
+m = 1
+a = 2
+r = 3
+d = 4
+i = 5
+print(m, a, r, d, i)
+print(type(m), type(a), type(r), type(d), type(i))
+переменная = 10
+print(переменная)
