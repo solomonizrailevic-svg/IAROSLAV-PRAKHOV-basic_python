26ed7270417f56ef6711c480c17d9bdcf3cd7bea
prakh <solomonizrailevic@gmail.com>
2026-02-01 14:20:08 +0300

    homework 2

---
+++ Pythonlesson_2/Homework_2_1.py
@@ -0,0 +1,45 @@
+# a = 4
+# b = 2
+# x = 2.3
+# y = 4.5
+# print(a)
+# print(b)
+# print(x)
+# print(y)
+# print(type(a))
+# print(type(b))
+# print(type(x))
+# print(type(y))
+
+# num1 = 2
+# num2 = 3
+# print(num1 + num2)
+# print(num1 - num2)
+# print(num1 * num2)
+# print(num1 / num2)
+# print(num1 // num2)
+# print(num1 % num2)
+# print(num1 ** num2)
+
+a = 10
+b = 3
+print(a / b)
+print(a // b)
+print(a % b)
+a = -10
+b = 3
+print(a / b)
+print(a // b)
+print(a % b)
+
+# print(5 + 3 * 2)
+# print((5 + 3) * 2)
+# print(10 / 2 ** 2)
+
+# count = 10
+# count += 5
+# count -= 3
+# count *= 2
+# count /= 4
+# print(count)
+
---
+++ Pythonlesson_2/Homework_2_2.py
@@ -0,0 +1,46 @@
+# s1 = "Python"
+# s2 = "Программирование"
+# print(s1, s2)
+# s3 = """
+# Nasilsiniz beyler
+# """
+# print(s3)
+# empty = ""
+# print(len(empty))
+#
+# first_name = "Иван"
+# last_name = "Петров"
+# full_name = first_name + " " + second_name
+# print(full_name)
+
+s = "Возраст"
+age = 25
+print(str(s) + str(age))
+
+# a = "ха"
+# print(a * 5)
+# print(a * 2.5)
+
+# text = "Привет, мир!"
+# print(len(text))
+# k = "   "
+# print(len(k))
+
+# sentence = "Я изучаю Python"
+# print("Python" in sentence)
+# print("Java" in sentence)
+
+# a = "apple"
+# b = "banana"
+# print(a > b)
+# print(a < b)
+# print(a == b)
+# print(a != b)
+# print(a >= b)
+# print(a <= b)
+#
+# print(ord("A"))
+# print(ord("a"))
+# print(ord("Я"))
+
+
